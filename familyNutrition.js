93570ace3bab00ce254b1bbb7b0c5f23a87d8b0e
GryffindorAdi <adipatiladitya2001@gmail.com>
2022-10-16 13:35:49 +0530

    final

---
+++ familyNutrition.js
@@ -0,0 +1,574 @@
+
+var data = [
+    {
+        discount: "32% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/967657/dr_morepen_pcos_powder_for_women_250_gm_0_0.jpg",
+        name: "Dr. Morepen PCOS Powder 250 gm",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Dr.Morepen Ltd",
+        best_price: "RS. 1019.32",
+        mrp_price: "Rs. 1499.00"
+    },
+    {
+        discount: "12% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/38061/b_protin_nutritional_powder_supplement_chocolate_500_gm_0.jpg",
+        name: "B-Protin Nutritional Powder Supplement - Chocolate 500 gm",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: British Biologicals",
+        best_price: "RS. 506.00",
+        mrp_price: "Rs. 575.00"
+    },
+    {
+        discount: "9% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/917110/aiwo_isabgol_psyllium_husk_200_gm_0_1.jpg",
+        name: "B-Protin Nutritional Powder Supplement - Pineapple 200 gm",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: British Biologicals",
+        best_price: "RS. 236.60",
+        mrp_price: "Rs. 260.00"
+    },
+    {
+      discount: "9% OFF",
+      image_url: "https://www.netmeds.com/images/product-v1/150x150/38059/b_protin_nutritional_powder_supplement_chocolate_200_gm_0.jpg",
+      name: "B-Protin Nutritional Powder Supplement - Chocolate 200 gm",
+      category: "Fitness",
+      sub_category: "Family Nutrition",
+      company_name: "Mkt: British Biologicals",
+      best_price: "RS. 236.60",
+      mrp_price: "Rs. 260.00"
+    },
+    {
+        discount: "40% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/937841/cf_mela_rest_melatonin_5mg_spray_50_ml_167276_0_5.jpg",
+        name: "CF Mela-Rest Melatonin 5mg Spray 50 ml",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Novus Life Sciences Pvt. Ltd.",
+        best_price: "RS. 299.40",
+        mrp_price: "Rs. 499.00"
+    },
+    {
+        discount: "18% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/937840/cf_mela_rest_melatonin_10mg_gummies_rasberry_flavour_60s_167269_0_6.jpg",
+        name: "CF Mela-Rest Melatonin 10mg Gummies - Rasberry Flavour 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Novus Life Sciences Pvt. Ltd.",
+        best_price: "RS. 287.00",
+        mrp_price: "Rs. 350.00"
+    },
+    {
+      discount: "9% OFF",
+      image_url: "https://www.netmeds.com/images/product-v1/150x150/373393/b_protin_nutritional_powder_supplement_mango_200_gm_0.jpg",
+      name: "B-Protin Nutritional Powder Supplement - Mango 200 gm",
+      category: "Fitness",
+      sub_category: "Family Nutrition",
+      company_name: "Mkt: British Biologicals",
+      best_price: "RS. 236.60",
+      mrp_price: "Rs. 260.00"
+    },
+    {
+        discount: "18% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/922865/cf_chelated_magnesium_glycinate_2408_mg_veg_tablet_60s_0_5.jpg",
+        name: "CF Chelated Magnesium Glycinate 2408 mg Veg Tablet 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Novus Life Sciences Pvt. Ltd.",
+        best_price: "RS. 287.00",
+        mrp_price: "Rs. 350.00"
+    },
+    {
+        discount: "18% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/922861/cf_nitric_oxide_supplement_1200_mg_veg_capsule_60s_137396_0_5.jpg",
+        name: "CF Nitric Oxide Supplement 1200 mg Veg Capsule 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Novus Life Sciences Pvt. Ltd.",
+        best_price: "RS. 287.00",
+        mrp_price: "Rs. 350.00"
+    },
+    {
+        discount: "28% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/922857/cf_testo_7x_1000_mg_veg_tablet_60s_0_5.jpg",
+        name: "CF Testo-7X 1000 mg Veg Tablet 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Novus Life Sciences Pvt. Ltd.",
+        best_price: "RS. 719.28",
+        mrp_price: "Rs. 999.00"
+    },
+    {
+        discount: "28% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/986333/dr_morepen_memory_focus_mood_health_supplement_tablet_60s_0_0.jpg",
+        name: "Dr. Morepen Milk Thistle+ Capsule 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Dr.Morepen Ltd",
+        best_price: "RS. 629.37",
+        mrp_price: "Rs. 350.00"
+    },
+    {
+      discount: "18% OFF",
+      image_url: "https://www.netmeds.com/images/product-v1/150x150/371628/b_protin_df_powder_dutch_chocolate_200_gm_0.jpg",
+      name: "B-Protin DF Powder - Dutch Chocolate 200 gm",
+      category: "Fitness",
+      sub_category: "Family Nutrition",
+      company_name: "Mkt: British Biologicals",
+      best_price: "RS. 287.00",
+      mrp_price: "Rs. 350.00"
+    },
+    {
+        discount: "20% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/967672/dr_morepen_ironsure_folic_acid_syrup_150_ml_0_0.jpg",
+        name: "Dr. Morepen Ironsure Folic Acid Syrup 150 ml",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Dr.Morepen Ltd",
+        best_price: "RS. 132.00",
+        mrp_price: "Rs. 165.00"
+    },
+    {
+        discount: "18% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/967654/dr_morepen_natural_collagen_builder_powder_250_gm_0_0.jpg",
+        name: "Dr. Morepen Natural Collagen Builder Powder 250 gm",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Dr.Morepen Ltd",
+        best_price: "RS. 854.43",
+        mrp_price: "Rs. 1499.00"
+    },
+    {
+        discount: "15% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/969634/enorma_urinary_tract_infection_tablet_60s_0_1.jpg",
+        name: "Enorma Urinary Tract Infection Tablet 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Esperer Onco Nutrtion Pvt Ltd",
+        best_price: "RS. 854.43",
+        mrp_price: "Rs. 1499.00"
+    },
+    {
+        discount: "15% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/969633/enorma_urinary_tract_infection_tablet_30s_0_1.jpg",
+        name: "Enorma Urinary Tract Infection Tablet 30's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Esperer Onco Nutrtion Pvt Ltd",
+        best_price: "RS. 905.50",
+        mrp_price: "Rs. 1070.00"
+    },
+    {
+        discount: "15% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/969632/enorma_obstructive_sleep_apnea_tablet_60s_0_1.jpg",
+        name: "Enorma Obstructive Sleep Apnea Tablet 60's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Esperer Onco Nutrtion Pvt Ltd",
+        best_price: "RS. 1245.25",
+        mrp_price: "Rs. 1465.00"
+    },
+    {
+        discount: "15% OFF",
+        image_url: "https://www.netmeds.com/images/product-v1/150x150/969631/enorma_obstructive_sleep_apnea_tablet_30s_0_1.jpg",
+        name: "Enorma Obstructive Sleep Apnea Tablet 30's",
+        category: "Fitness",
+        sub_category: "Family Nutrition",
+        company_name: "Mkt: Esperer Onco Nutrtion Pvt Ltd",
+        best_price: "RS. 850.00",
+        mrp_price: "Rs. 1000.00"
+    }
+]
+
+var datafromlocal = JSON.parse(localStorage.getItem("newcart")) || [];
+DisplayDressing(data);
+function DisplayDressing(data) {
+  document.querySelector("#dressing").innerHTML = "";
+  data.map(function (ele, i) {
+    var div = document.createElement("div");
+    var discount = document.createElement("p");
+    var dis_span = document.createElement("span");
+    dis_span.innerText = ele.discount;
+    discount.append(dis_span);
+
+    var image = document.createElement("img");
+    image.setAttribute("src", ele.image_url);
+
+    var name = document.createElement("h4");
+    name.innerText = ele.name;
+
+    var para = document.createElement("p");
+    var span1 = document.createElement("span");
+    span1.innerText = ele.category;
+    var span2 = document.createElement("span");
+    span2.innerText = ele.sub_category;
+    para.append(span1, span2);
+
+    var company_name = document.createElement("p");
+    company_name.innerText = ele.company_name;
+
+    var best_price = document.createElement("h4");
+    sub_best = document.createElement("span");
+    sub_best.innerText = "Best Price";
+    sub_best_price = document.createElement("span");
+    sub_best_price.innerText = ele.best_price;
+    best_price.append(sub_best, sub_best_price);
+
+    var mrp_price = document.createElement("h5");
+    sub_mrp = document.createElement("span");
+    sub_mrp.innerText = "MRP Price";
+    sub_mrp_price = document.createElement("span");
+    sub_mrp_price.innerText = ele.mrp_price;
+    mrp_price.append(sub_mrp, sub_mrp_price);
+    var button = document.createElement("button");
+    button.innerText = "ADD TO CART";
+    button.setAttribute("class", "cartbtn");
+    button.addEventListener("click", function () {
+      addcart(ele.id, i);
+    });
+
+    div.append(
+      discount,
+      image,
+      name,
+      para,
+      company_name,
+      best_price,
+      mrp_price,
+      button
+    );
+    document.querySelector("#dressing").append(div);
+    localStorage.setItem("dressings", JSON.stringify(data));
+  });
+}
+// newe cart making
+
+var id = 500;
+data.map((elem) => {
+  elem.id = id;
+  id++;
+});
+
+var productIncart = JSON.parse(localStorage.getItem("newcart")) || [];
+console.log(productIncart);
+function addcart(id, i) {
+  if (productIncart.length == 0) {
+    data[i].qty = +1;
+    productIncart.push(data[i]);
+  } else {
+    var flag = false;
+    productIncart.map((e) => {
+      if (e.id == id) {
+        e.qty = +e.qty + 1;
+        flag = true;
+      }
+    });
+
+    if (!flag) {
+      data[i].qty = +1;
+      productIncart.push(data[i]);
+    }
+  }
+
+  // productIncart.push(product[0]);
+  localStorage.setItem("newcart", JSON.stringify(productIncart));
+}
+
+// new cart end
+
+//-------------------cart-------------------- //
+
+var productincart=JSON.parse(localStorage.getItem("newcart")) || [];
+function addToCart(index){
+  var product=data.filter(function(element,i){
+    return i==index;
+  })
+  productincart.push(product[0]);
+  localStorage.setItem("cartproduct",JSON.stringify(productincart))
+}
+// var num =dressing[0].best_price.slice(4,);
+// num =parseInt(num)
+// console.log(typeof num);
+function HighToLow() {
+  console.log("hightolow")
+    data.sort(function (x, y) {
+        console.log(parseInt(x.best_price.slice(4,)) + " " + typeof parseInt(x.best_price.slice(4,)))
+        return parseInt(y.best_price.slice(4,)) - parseInt(x.best_price.slice(4,));
+    });
+    DisplayDressing(data);
+}
+function LowToHigh() {
+    data.sort(function (x, y) {
+        console.log(parseInt(x.best_price.slice(4,)) + " " + typeof parseInt(x.best_price.slice(4,)))
+        return parseInt(x.best_price.slice(4,)) - parseInt(y.best_price.slice(4,));
+    });
+    DisplayDressing(data);
+}
+function sortByDiscount() {
+    data.sort(function (x, y) {
+        console.log(parseInt(x.discount.slice(0, 2)) + " " + typeof parseInt(x.best_price.slice(4,)))
+        return parseInt(y.discount.slice(0, 2)) - parseInt(x.discount.slice(0, 2));
+    });
+    DisplayDressing(data);
+}
+console.log(dressing)
+
+ // -----------------checkBox Brand Logic----------------------------------
+ var a=b=c=d=e=f=g=h=0; var count1=0;
+ function funclick1(){
+     if(count1==0){
+         a=1;
+         count1++;
+     }else{
+         a=0;
+         count1--;
+     }
+
+     abc1();
+ }
+ var count2=0;
+ function funclick2(){
+     if(count2==0){
+         b=1;
+         count2++;
+     }else{
+         b=0;
+         count2--;
+     }
+     abc2();
+ }
+ var count3=0;
+ function funclick3(){
+     if(count3==0){
+         c=1;
+         count3++;
+     }else{
+         c=0;
+         count3--;
+     }
+     abc3();
+ }
+ var count4=0;
+ function funclick4(){
+     if(count4==0){
+         d=1;
+         count4++;
+     }else{
+         d=0;
+         count4--;
+     }
+     abc4();
+ }
+
+ var count5=0;
+ function funclick5(){
+     if(count5==0){
+         e=1;
+         count5++;
+     }else{
+         e=0;
+         count5--;
+     }
+     abc5();
+ }
+ var count6=0;
+ function funclick6(){
+     if(count6==0){
+         f=1;
+         count6++;
+     }else{
+         f=0;
+         count6--;
+     }
+     abc6();
+ }
+ var count7=0;
+ function funclick7(){
+     if(count7==0){
+         g=1;
+         count7++;
+     }else{
+         g=0;
+         count7--;
+     }
+     abc7();
+ }
+ var count8=0;
+ function funclick8(){
+     if(count8==0){
+         h=1;
+         count8++;
+     }else{
+         h=0;
+         count8--;
+     }
+     abc8();
+ }
+ var checkBoxArr=[];
+  ///////////////////////////////////////////
+function abc1(){
+   if(a==1){
+     var w=[]
+     data.map(function(ele){
+       if(ele.company_name=="MKT: 3M india Ltd"){
+
+        w.push(ele);
+         DisplayDressing(w);
+       }
+     })
+   }else{
+     DisplayDressing(data);
+   }
+  }
+function abc2(){
+   if(b==1){
+     var x=[]
+     data.map(function(ele){
+        if(ele.company_name=="MKT: Prime Medica Promotion Pvt.Ltd"){
+          x.push(ele);
+          DisplayDressing(x);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+function abc3(){
+    if(c==1){
+     var y=[];
+     data.map(function(ele){
+        if(ele.company_name=="Ramaraju Surgical Cotton Mills Ltd"){
+          y.push(ele);
+          DisplayDressing(y);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+function abc4(){
+    if(d==1){
+      var z=[];
+      data.map(function(ele){
+        if(ele.company_name=="MKT: Zywie Pharmaceutical Pvt.Ltd"){
+          z.push(ele);
+          DisplayDressing(z);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+//--------------------- End of CheckBox Brand----------------------------
+
+function abc5(){
+    if(e==1){
+      var w=[];
+      data.map(function(ele){
+        if(ele.company_name=="Mkt: Johnson & Johnson Pvt Ltd"){
+          w.push(ele);
+          DisplayDressing(w);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+
+function abc6(){
+    if(f==1){
+      var z=[];
+      data.map(function(ele){
+        if(ele.company_name=="MKT: Datt Mediproducts Pvt.Ltd"){
+          z.push(ele);
+          DisplayDressing(z);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+
+function abc7(){
+    if(g==1){
+      var z=[];
+      data.map(function(ele){
+        if(ele.company_name=="Mkt: Essity India Pvt Ltd"){
+          z.push(ele);
+          DisplayDressing(z);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+
+function abc8(){
+    if(h==1){
+      var z=[];
+      data.map(function(ele){
+        if(ele.company_name=="MKT: 3M india Ltd"){
+          z.push(ele);
+          DisplayDressing(z);
+        }
+      })
+    }else{
+      DisplayDressing(data);
+    }
+   }
+
+   ///////////////////////////////////////////////////////////
+
+   function showVal(value) {
+    console.log("input value:" + Number(value));
+    let v = Number(value * 10);
+    document.querySelector("#kl").innerHTML = v;
+    //   console.log(());
+
+    if (value == 0) {
+      console.log("cancel selected");
+      // console.log(dressing.length);
+    } else if (value == 100) {
+      console.log("ok selected");
+    }
+
+    //   dressing.map(function (el) {
+    //     console.log(el.mrp_price);
+    //     return el;
+    //   });
+    //   DisplayDressing(arr);
+  }
+  document.querySelector('input[type="range"]').ontouchend = function () {
+    // clearInterval(document.init);
+    var value = Number(this.value);
+    if (value > 98) {
+      console.log("ok selected");
+      document.getElementById("rangeElement").disabled = true;
+      return;
+    } else if (value < 2) {
+      console.log("cancel selected");
+      document.getElementById("rangeElement").disabled = true;
+      return;
+    } else {
+      //console.log("reset");
+      if (value >= 50) {
+        var rangeAnimIntervel = setInterval(function () {
+          document.querySelector('input[type="range"]').value = value--;
+          if (value == 50) {
+            clearInterval(rangeAnimaIntervel);
+            return;
+          }
+        }, 1);
+      } else {
+        var rangeAnimIntervel = setInterval(function () {
+          document.querySelector('input[type="range"]').value = value++;
+          if (value == 50) {
+            clearInterval(rangeAnimaIntervel);
+            return;
+          }
+        }, 1);
+      }
+    }
+  };
